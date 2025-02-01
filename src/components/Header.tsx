6c98045d2b4a754f182d9cd5cc1ef687be55c11e
aBacIdEe <91438467+aBacIdEe@users.noreply.github.com>
2025-02-01 02:02:49 -0500

    fancy animated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 // src/components/Header.tsx
-import { useState, FC } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, FC } from "react";
+import { Link } from "react-router-dom";
 
 const Header: FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -11,7 +11,7 @@ const Header: FC = () => {
 
   return (
     <header className="flex bg-black text-white">
-      <nav className="flex flex-row items-center justify-between mx-8 w-full h-28">
+      <nav className="flex flex-row items-center justify-between mx-8 w-full h-24">
         {/* Logo / Brand */}
         <div className="flex-1 text-xl font-bold w-1/3">
           <Link to="/" className="flex items-center">
@@ -22,23 +22,65 @@ const Header: FC = () => {
 
         {/* Desktop Menu */}
         <div className="flex flex-row justify-between w-1/3 h-full">
-          <Link className="flex-1 items-center justify-center" to="/">Home</Link>
-          <Link className="flex-1 text-center" to="/about">About</Link>
-          <Link className="flex-1 text-center" to="/summer">Summer Lectures</Link>
-          <Link className="flex-1 text-center" to="/archive">Archive</Link>
-          <Link className="flex-1 text-center" to="/resources">Resources</Link>
-          <Link className="flex-1 text-center" to="/awards">Awards</Link>
+          <Link className="flex-1 group relative z-0" to="/">
+            <span className="flex flex-col items-center justify-center h-full overflow-hidden">
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                Home
+              </span>
+            </span>
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
+          </Link>
+          <Link className="flex-1 group relative z-0" to="/about">
+            <span className="flex flex-col items-center justify-center h-full overflow-hidden">
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                About
+              </span>
+            </span>
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
+          </Link>
+          <Link className="flex-1 group relative z-0" to="/summer">
+            <span className="flex flex-col items-center justify-center h-full overflow-hidden">
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                Summer Lectures
+              </span>
+            </span>
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
+          </Link>
+          <Link className="flex-1 group relative z-0" to="/archive">
+            <span className="flex flex-col items-center justify-center h-full overflow-hidden">
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                Archive
+              </span>
+            </span>
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
+          </Link>
+          <Link className="flex-1 group relative z-0" to="/resources">
+            <span className="flex flex-col items-center justify-center h-full overflow-hidden">
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                Resources
+              </span>
+            </span>
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
+          </Link>
           <Link className="flex-1 group relative z-0" to="/awards">
             <span className="flex flex-col items-center justify-center h-full overflow-hidden">
-              <span className="absolute inset-0 aspect-square w-full scale-500 origin-center -translate-y-full bg-yellow-500 transition-all duration-1000 group-hover:-translate-y-0 group-hover:scale-500"></span>
-              <span className="relative z-10 text-white transition-colors duration-500 mix-blend-difference">Awards</span>
-              {/* Darker Duplicate Layer for Stacking Effect */}
-              <span className="absolute z-10 text-black mix-blend-overlay">Awards</span>
-              <span className="absolute z-10 text-black mix-blend-overlay">Awards</span>
-              <span className="absolute z-10 text-black mix-blend-overlay">Awards</span>
+              <span className="relative z-10 text-transparent bg-white bg-clip-text transition-all duration-300 group-hover:bg-black">
+                Awards
+              </span>
             </span>
-            
-
+            <span className="absolute inset-0 overflow-hidden">
+              <span className="absolute inset-0 aspect-square w-full scale-[1.5] origin-center -translate-y-[150%] bg-yellow-500 transition-all duration-600 group-hover:-translate-y-0 group-hover:scale-[1.5]"></span>
+            </span>
           </Link>
         </div>
 
@@ -57,13 +99,41 @@ const Header: FC = () => {
       {/* Mobile Menu (dropdown) */}
       {isOpen && (
         <ul className="md:hidden bg-black text-white">
-          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-          <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
-          <li><Link to="/summer" onClick={toggleMenu}>Summer Lectures</Link></li>
-          <li><Link to="/research" onClick={toggleMenu}>Research</Link></li>
-          <li><Link to="/archive" onClick={toggleMenu}>Archive</Link></li>
-          <li><Link to="/resources" onClick={toggleMenu}>Resources</Link></li>
-          <li><Link to="/awards" onClick={toggleMenu}>Awards</Link></li>
+          <li>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={toggleMenu}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/summer" onClick={toggleMenu}>
+              Summer Lectures
+            </Link>
+          </li>
+          <li>
+            <Link to="/research" onClick={toggleMenu}>
+              Research
+            </Link>
+          </li>
+          <li>
+            <Link to="/archive" onClick={toggleMenu}>
+              Archive
+            </Link>
+          </li>
+          <li>
+            <Link to="/resources" onClick={toggleMenu}>
+              Resources
+            </Link>
+          </li>
+          <li>
+            <Link to="/awards" onClick={toggleMenu}>
+              Awards
+            </Link>
+          </li>
         </ul>
       )}
     </header>
