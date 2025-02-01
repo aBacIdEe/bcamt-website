74961a76b172b04dbe26063ebca391395b3513f4
aBacIdEe <91438467+aBacIdEe@users.noreply.github.com>
2025-01-31 23:19:53 -0500

    file restructuring + default font = computer modern

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        CM: ['Computer Modern', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
